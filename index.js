aa31a9fc48884aa56b65472a0458d32786b9355a
yeasinbinali <yeasinbinali@gmail.com>
2024-05-04 14:55:48 +0600

    services collection added

--- index.js
+++ index.js
@@ -14,21 +14,30 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    await client.connect();
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // await client.close();
-  }
+    try {
+
+        const serviceCollection = client.db('AutoCareDB').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const services = serviceCollection.find();
+            const result = await services.toArray();
+            res.send(result);
+        })
+
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
